bbbdc2365ca71bf70f499f2ff8f3d3372b66ad98
Josetruck <olemiluis@gmail.com>
2022-11-19 18:31:52 +0100

    Order creados

---
+++ controllers/order.controllers.js
@@ -0,0 +1,16 @@
+const Users = require("../models/user.model");
+const Address = require("../models/address.model");
+const Orders = require("../models/order.model");
+const Orders_Instruments = require("../models/order_instrument.model");
+
+const sendemail = require("./email.controllers");
+const SendmailTransport = require("nodemailer/lib/sendmail-transport");
+const order = {
+    new_order_pickup: (req,res)=>{
+        
+    },    
+    new_order_address: (req,res)=>{
+
+    },
+}
+module.exports = order
