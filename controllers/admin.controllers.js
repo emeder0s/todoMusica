670ede761460bdf7d235417cd4acde5805ccd815
Josetruck <olemiluis@gmail.com>
2022-11-22 11:44:11 +0100

    pre entrega

--- controllers/admin.controllers.js
+++ controllers/admin.controllers.js
@@ -8,10 +8,8 @@ const jwt = require("jsonwebtoken");
 const _admin = {
     /**
      * Login del administrador
-
      * @param {JSON} req 
      * @param {JSON} res 
-
      */
     login: async (req, res) => {
         const { user, password } = req.body;
@@ -33,10 +31,8 @@ const _admin = {
 
     /**
      * Comprueba si el adinistrador está logueado o no 
-
      * @param {JSON} req 
      * @param {JSON} res 
-
      */
     isAdminAuthorized: (req, res) => {
         var cookies = req.cookies;
@@ -49,23 +45,27 @@ const _admin = {
                 res.json(false)
             }
         }
+    },
+    /**
+     * Función que permite al Admin actuar como si fuera un usuario a partir de su dirección de email.
+     * @param {JSON} req 
+     * @param {JSON} res 
+     */
+    loginAsUser: async (req, res) => {
+        const { email } = req.body;
+        const user = await Users.findOne({ where: { "email": req.body.email } });
+        const infoJwt = jwt.sign({ email }, "m1c4s4", {
+            expiresIn: "1800s",
+        });
+        if (user) {
+            res.cookie("infoJwt", infoJwt);
+            res.json("ok")
+        } else {
+            res.json("no ok")
+        }
 
-      },
 
-      /**
-   * Log out del admin - limpia la cookie con el json web token del navegador
-   * @param {json} req 
-   * @param {json} res 
-   */
-  logout: (req, res) => {
-    var cookies = req.cookies;
-    if (cookies) {
-      var token = cookies.infoJwt;
-      res.json(token);
-    }else{
-        res.json(false)
     }
-  }
 }
 
 
